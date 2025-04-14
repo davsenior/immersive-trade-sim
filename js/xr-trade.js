c7e5508f978741b6729d5994f38133a24194ad16
davsenior <david.senior@mygeorgian.ca>
2025-04-14 00:25:28 -0400

    reverted to working copy

--- js/xr-trade.js
+++ js/xr-trade.js
@@ -6,24 +6,28 @@ const createScene = async function () {
     const scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3(0.5, 0.5, 0.5);
 
+    // Add camera
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
+    camera.attachControl(canvas, true);
+
     // Add lighting
     const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    // Create ground
+    // Create ground with grass texture
     const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
     const grassMaterial = new BABYLON.StandardMaterial("grassMaterial", scene);
     grassMaterial.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/textures/grass.png", scene);
     ground.material = grassMaterial;
     ground.receiveShadows = true;
 
-    // Wall 1 (left)
+    // Wall material
     const wallMaterial = new BABYLON.StandardMaterial("wallMaterial", scene);
     wallMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
 
+    // Wall 1 (left)
     const wall1 = BABYLON.MeshBuilder.CreateBox("wall1", { width: 0.1, height: 2, depth: 10 }, scene);
-    wall1.position.x = -5;
-    wall1.position.y = 1;
+    wall1.position.set(-5, 1, 0);
     wall1.material = wallMaterial;
 
     // Wall 2 (right)
@@ -32,8 +36,7 @@ const createScene = async function () {
 
     // Wall 3 (back)
     const wall3 = BABYLON.MeshBuilder.CreateBox("wall3", { width: 10, height: 2, depth: 0.1 }, scene);
-    wall3.position.z = -5;
-    wall3.position.y = 1;
+    wall3.position.set(0, 1, -5);
     wall3.material = wallMaterial;
 
     // Cutting table
@@ -47,21 +50,17 @@ const createScene = async function () {
     // Table legs
     const legHeight = 0.75;
     const legSize = 0.1;
+    const createLeg = (x, z) => {
+        const leg = BABYLON.MeshBuilder.CreateBox("leg", { width: legSize, height: legHeight, depth: legSize }, scene);
+        leg.position.set(x, legHeight / 2, z);
+        leg.material = tableMat;
+    };
+    createLeg(-0.9, -0.4);
+    createLeg(0.9, -0.4);
+    createLeg(-0.9, 0.4);
+    createLeg(0.9, 0.4);
 
-    const leg1 = BABYLON.MeshBuilder.CreateBox("leg1", { width: legSize, height: legHeight, depth: legSize }, scene);
-    leg1.position.set(-0.9, legHeight / 2, -0.4);
-    leg1.material = tableMat;
-
-    const leg2 = leg1.clone("leg2");
-    leg2.position.x = 0.9;
-
-    const leg3 = leg1.clone("leg3");
-    leg3.position.z = 0.4;
-
-    const leg4 = leg2.clone("leg4");
-    leg4.position.z = 0.4;
-
-    // Table support beam
+    // Support
     const support = BABYLON.MeshBuilder.CreateBox("support", { width: 2, height: 0.05, depth: 0.1 }, scene);
     support.position.set(0, 0.4, 0);
     support.material = tableMat;
@@ -78,60 +77,33 @@ const createScene = async function () {
     saw.material.diffuseColor = new BABYLON.Color3(0.7, 0.7, 0.7);
     saw.position.set(0, 1, 1);
 
-    // Enable WebXR
+    // Enable WebXR (VR)
     const xrHelper = await scene.createDefaultXRExperienceAsync({
-        uiOptions: {
-            sessionMode: "immersive-vr",
-        },
+        uiOptions: { sessionMode: "immersive-vr" },
     });
 
-    const featuresManager = xrHelper.baseExperience.featuresManager;
-
-    // Enable teleportation
-    featuresManager.enableFeature(BABYLON.WebXRFeatureName.TELEPORTATION, "latest", {
-        xrInput: xrHelper.input,
-        floorMeshes: [ground],
-    });
-
-    let cutPlank = false;
-
-    // Add interaction
+    // Input for grabbing plank and saw
     xrHelper.input.onControllerAddedObservable.add((controller) => {
         const motionController = controller.motionController;
-        if (!motionController) return;
+        if (motionController) {
+            const selectComponent = motionController.getComponent("xr-standard-trigger");
 
-        const trigger = motionController.getComponent("xr-standard-trigger");
+            selectComponent.onButtonStateChangedObservable.add(() => {
+                if (selectComponent.pressed) {
+                    const distanceToPlank = BABYLON.Vector3.Distance(controller.grip.position, plank.position);
+                    const distanceToSaw = BABYLON.Vector3.Distance(controller.grip.position, saw.position);
 
-        trigger.onButtonStateChangedObservable.add(() => {
-            if (trigger.pressed) {
-                const gripPos = controller.grip.position;
-
-                const distPlank = BABYLON.Vector3.Distance(gripPos, plank.position);
-                const distSaw = BABYLON.Vector3.Distance(gripPos, saw.position);
-
-                if (distPlank < 0.5) plank.setParent(controller.grip);
-                else if (distSaw < 0.5) saw.setParent(controller.grip);
-
-                if (distPlank < 0.3 && distSaw < 0.3 && !cutPlank) {
-                    cutPlank = true;
-
-                    const cutAnim = new BABYLON.Animation("cut", "scaling.x", 30,
-                        BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-                        BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
-
-                    cutAnim.setKeys([
-                        { frame: 0, value: plank.scaling.x },
-                        { frame: 15, value: plank.scaling.x / 2 }
-                    ]);
-
-                    plank.animations.push(cutAnim);
-                    scene.beginAnimation(plank, 0, 15, false);
+                    if (distanceToPlank < 0.5) {
+                        plank.setParent(controller.grip);
+                    } else if (distanceToSaw < 0.5) {
+                        saw.setParent(controller.grip);
+                    }
+                } else {
+                    plank.setParent(null);
+                    saw.setParent(null);
                 }
-            } else {
-                plank.setParent(null);
-                saw.setParent(null);
-            }
-        });
+            });
+        }
     });
 
     return scene;
@@ -143,7 +115,6 @@ createScene().then((scene) => {
         scene.render();
     });
 });
-
 window.addEventListener("resize", () => {
     engine.resize();
 });
